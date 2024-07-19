b4b5290cf64d012a6116c0a9b1e85e91f501f872
mshdabiola <mshdabiola@gmail.com>
2024-07-19 11:50:19 +0100

    Test: Add SettingScreenTest and SettingViewModelTest
    This commit adds two new test files for the Setting feature:
    
    - `SettingScreenTest.kt`:
     Contains UI tests for the `SettingScreen` composable.
    - `SettingViewModelTest.kt`: Contains unit tests for the `SettingViewModel`.
    
    These tests provide initial test coverage for the Setting feature and can be expanded upon as the feature evolves.

---
+++ features/setting/src/jvmTest/kotlin/com/mshdabiola/setting/SettingScreenTest.kt
@@ -0,0 +1,20 @@
+package com.mshdabiola.setting
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.mshdabiola.model.UserData
+import org.junit.Rule
+import org.junit.Test
+
+class SettingScreenTest {
+
+    @get:Rule
+    val composeRule= createComposeRule()
+
+    @Test
+    fun main(){
+        composeRule.setContent {
+          //  SettingScreen(settingState = SettingState(UserData()))
+        }
+
+    }
+}
---
+++ features/setting/src/jvmTest/kotlin/com/mshdabiola/setting/SettingViewModelTest.kt
@@ -0,0 +1,41 @@
+/*
+ *abiola 2022
+ */
+
+package com.mshdabiola.setting
+
+import com.mshdabiola.testing.repository.TestNoteRepository
+import com.mshdabiola.testing.repository.TestUserDataRepository
+import com.mshdabiola.testing.util.MainDispatcherRule
+import com.mshdabiola.testing.util.TestAnalyticsHelper
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * To learn more about how this test handles Flows created with stateIn, see
+ * https://developer.android.com/kotlin/flow/test#statein
+ */
+class SettingViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val analyticsHelper = TestAnalyticsHelper()
+    private val userDataRepository = TestUserDataRepository()
+    private val noteRepository = TestNoteRepository()
+
+//    private val savedStateHandle = SavedStateHandle(mapOf(DETAIL_ID_ARG to 4))
+    private lateinit var viewModel: SettingViewModel
+
+    @Before
+    fun setup() {
+        viewModel = SettingViewModel(
+            userDataRepository = userDataRepository
+        )
+    }
+
+    @Test
+    fun stateIsInitiallyLoading() = runTest {
+    }
+}
