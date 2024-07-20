5776d4afeebc1cf9297b6cdc5acd68f8305dbb34
mshdabiola <mshdabiola@gmail.com>
2024-07-20 01:13:22 +0100

    Refactor: Remove unused navigation code
    This commit removes unused navigation code related to the `MAIN_ROUTE` and simplifies the navigation logic in `SkApp.
    kt` and `SkNavHost.kt`.
    
    - Removed the `MAIN_ROUTE` constant and its associated navigation logic.
    - Updated the `SkNavHost` to start at the `Main` destination.
    - Removed unused parameters in `mainScreen`, `detailScreen`, and `settingScreen`
     composables.

--- app/src/commonMain/kotlin/com/mshdabiola/skeletonapp/navigation/SkNavHost.kt
+++ app/src/commonMain/kotlin/com/mshdabiola/skeletonapp/navigation/SkNavHost.kt
@@ -9,8 +9,8 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import com.mshdabiola.detail.navigation.detailScreen
 import com.mshdabiola.detail.navigation.navigateToDetail
-import com.mshdabiola.main.navigation.MAIN_ROUTE
 import com.mshdabiola.main.navigation.mainScreen
+import com.mshdabiola.model.naviagation.Main
 import com.mshdabiola.setting.navigation.navigateToSetting
 import com.mshdabiola.setting.navigation.settingScreen
 import com.mshdabiola.skeletonapp.ui.SkAppState
@@ -20,28 +20,23 @@ fun SkNavHost(
     appState: SkAppState,
     onShowSnackbar: suspend (String, String?) -> Boolean = { _, _ -> false },
     modifier: Modifier = Modifier,
-    startDestination: String = MAIN_ROUTE,
 ) {
     val navController = appState.navController
     NavHost(
         navController = navController,
-        startDestination = startDestination,
+        startDestination = Main,
         modifier = modifier,
     ) {
         mainScreen(
-            screenSize = appState.screenSize,
             onShowSnack = onShowSnackbar,
-            onClicked = navController::navigateToDetail,
             navigateToSetting = navController::navigateToSetting,
             navigateToDetail = navController::navigateToDetail,
         )
         detailScreen(
-            screenSize = appState.screenSize,
             onShowSnack = onShowSnackbar,
             onBack = navController::popBackStack,
         )
         settingScreen(
-            screenSize = appState.screenSize,
             onShowSnack = onShowSnackbar,
             onBack = navController::popBackStack,
         )
