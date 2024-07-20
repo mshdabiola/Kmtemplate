5d22a0fb8cfb446df74d5c52f6cafa1fb4bd4179
mshdabiola <mshdabiola@gmail.com>
2024-07-21 00:02:55 +0100

    feat: Add SharedContentPreview composable
    This commit introduces a new composable function called `SharedContentPreview` to provide a shared transition scope and animated content
     scope for previewing composables.
    
    - The `SharedContentPreview` composable is added to the `ui` module.
    - It is used in the `DetailScreenTest` and `MainScreenTest` to wrap the previewed composables.
    - This allows for testing and previewing composables that
     utilize shared transitions and animated content.

--- features/main/src/jvmTest/kotlin/com/mshdabiola/main/MainScreenTest.kt
+++ features/main/src/jvmTest/kotlin/com/mshdabiola/main/MainScreenTest.kt
@@ -1,7 +1,9 @@
 package com.mshdabiola.main
 
+import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.mshdabiola.data.model.Result
+import com.mshdabiola.ui.SharedContentPreview
 import org.junit.Rule
 import org.junit.Test
 
@@ -10,10 +12,18 @@ class MainScreenTest {
     @get:Rule
     val composeRule = createComposeRule()
 
+    @OptIn(ExperimentalSharedTransitionApi::class)
     @Test
     fun main() {
         composeRule.setContent {
-            MainScreen(mainState = Result.Loading)
+            SharedContentPreview { sharedTransitionScope, animatedContentScope ->
+                MainScreen(
+                    mainState = Result.Loading,
+                    sharedTransitionScope = sharedTransitionScope,
+                    animatedContentScope = animatedContentScope
+                )
+            }
+
         }
     }
 }
