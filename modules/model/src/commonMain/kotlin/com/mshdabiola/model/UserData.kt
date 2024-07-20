be032872ad62fcfffd9445010fe16ff768eebe8d
mshdabiola <mshdabiola@gmail.com>
2024-07-20 23:45:10 +0100

    feat: Add default values to UserData
    This commit adds default values to the properties of the `UserData` data class.
    
    - `themeBrand` now defaults
     to `ThemeBrand.DEFAULT`.
    - `darkThemeConfig` now defaults to `DarkThemeConfig.DARK`.
    - `useDynamicColor` now defaults to `true`.
    - `shouldHideOnboarding` now defaults to `false`.

--- modules/model/src/commonMain/kotlin/com/mshdabiola/model/UserData.kt
+++ modules/model/src/commonMain/kotlin/com/mshdabiola/model/UserData.kt
@@ -8,8 +8,8 @@ package com.mshdabiola.model
  * Class summarizing user interest data
  */
 data class UserData(
-    val themeBrand: ThemeBrand,
-    val darkThemeConfig: DarkThemeConfig,
-    val useDynamicColor: Boolean,
-    val shouldHideOnboarding: Boolean,
+    val themeBrand: ThemeBrand = ThemeBrand.DEFAULT,
+    val darkThemeConfig: DarkThemeConfig = DarkThemeConfig.DARK,
+    val useDynamicColor: Boolean = true,
+    val shouldHideOnboarding: Boolean = false,
 )
