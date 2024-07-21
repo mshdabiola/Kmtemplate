5097cc47fe92a6179c001078d18e14dc5e93e05e
mshdabiola <mshdabiola@gmail.com>
2024-07-21 03:24:45 +0100

    Refactor: Move Setting route to Setting feature
    This commit moves the `Setting` route from the `model` module to the `Setting` feature to improve
     modularity and organization.
    
    - The `Setting` route is now located in the `com.mshdabiola.setting.navigation` package.
    - Removed unused import of `ExperimentalSharedTransitionApi` in `SettingNavigation.kt`.

--- features/setting/src/commonMain/kotlin/com/mshdabiola/setting/navigation/SettingNavigation.kt
+++ features/setting/src/commonMain/kotlin/com/mshdabiola/setting/navigation/SettingNavigation.kt
@@ -4,21 +4,20 @@
 
 package com.mshdabiola.setting.navigation
 
-import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.dialog
-import com.mshdabiola.model.naviagation.Setting
 import com.mshdabiola.setting.SettingRoute
 import com.mshdabiola.setting.SettingViewModel
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
 
-fun NavController.navigateToSetting(navOptions: NavOptions = androidx.navigation.navOptions { }) = navigate(Setting, navOptions)
+fun NavController.navigateToSetting(navOptions: NavOptions = androidx.navigation.navOptions { }) = navigate(
+    Setting, navOptions)
 
-@OptIn(KoinExperimentalAPI::class, ExperimentalSharedTransitionApi::class)
+@OptIn(KoinExperimentalAPI::class)
 fun NavGraphBuilder.settingScreen(
     modifier: Modifier,
     onShowSnack: suspend (String, String?) -> Boolean,
