89cce7b541261857d079c8b2a6eff023a0e11555
mshdabiola <mshdabiola@gmail.com>
2024-08-25 18:08:30 +0100

    chore: Remove unused import
    Removed unused import of `androidx.compose.ui.geometry.Size` from `SkAppState.kt`.

--- app/src/jvmTest/kotlin/com/mshdabiola/skeletonapp/SkAppState.kt
+++ app/src/jvmTest/kotlin/com/mshdabiola/skeletonapp/SkAppState.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSiz
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.unit.DpSize
