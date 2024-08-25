9ac75f7fb283f0e1779dca97ef05fed3121c6e68
mshdabiola <mshdabiola@gmail.com>
2024-08-25 17:56:50 +0100

    test: Update WindowSizeClass calculation in SkAppState test
    The calculation of WindowSizeClass in the SkAppState test has been updated to use DpSize instead of
     Size and density.
    This change improves the accuracy and consistency of the test by using density-independent units.

--- app/src/jvmTest/kotlin/com/mshdabiola/skeletonapp/SkAppState.kt
+++ app/src/jvmTest/kotlin/com/mshdabiola/skeletonapp/SkAppState.kt
@@ -7,6 +7,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.createGraph
@@ -42,8 +44,7 @@ class SkAppState {
                     navController = navController,
                     coroutineScope = backgroundScope,
                     windowSizeClass = WindowSizeClass.calculateFromSize(
-                        size = Size(456f, 3f),
-                        density = density,
+                        size = DpSize(456.dp, 789.dp),
                     ),
 
                 )
