ee0546f0d4968326d77aef000a26fe4020b31294
mshdabiola <mshdabiola@gmail.com>
2024-11-29 11:10:12 +0100

    chore: Release version 1.2.2
    This commit updates the version code to 122 and the version name to 1.2.2.
    Additionally, it changes the displayed version of the application window.

--- app/src/jvmMain/kotlin/com/mshdabiola/skeletonapp/MainApp.kt
+++ app/src/jvmMain/kotlin/com/mshdabiola/skeletonapp/MainApp.kt
@@ -36,7 +36,7 @@ fun mainApp() {
             position = WindowPosition.Aligned(Alignment.Center),
         )
 
-        val version = "1.1.5"
+        val version = "1.2.2"
         Window(
             onCloseRequest = ::exitApplication,
             title = "$appName v$version",
