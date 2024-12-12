7f9c2c0115ba14d1efd63ffd5675062eec75565c
mshdabiola <mshdabiola@gmail.com>
2024-12-12 22:01:05 +0100

    refactor: Update RealModelRepository and DataModule
    This commit updates the `RealModelRepository` and `DataModule` to support multiplatform.
    
    Specifically, the following changes were made:
    
    - Moved `RealModelRepository` from `commonMain` to `nonJsMain` and `wasmJsMain`.
    - Created a common `DataModule` and platform-specific `DataModule` implementations for `nonJsMain` and `wasmJsMain`.
    - Updated dependencies and imports to reflect the new structure.

---
+++ modules/data/src/wasmJsMain/kotlin/com/mshdabiola/data/repository/RealModelRepository.kt
@@ -0,0 +1,42 @@
+package com.mshdabiola.data.repository
+
+import com.mshdabiola.model.Note
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.updateAndGet
+import kotlinx.coroutines.withContext
+
+internal class RealModelRepository(
+    private val ioDispatcher: CoroutineDispatcher,
+) : NoteRepository {
+    private val notes = MutableStateFlow(listOf<Note>())
+    override suspend fun upsert(note: Note): Long {
+        return withContext(ioDispatcher) {
+            notes.updateAndGet {
+                it.toMutableList().apply { add(note) }
+            }.count().toLong()
+        }
+    }
+
+    override fun getAll(): Flow<List<Note>> {
+        return notes
+    }
+
+    override fun getOne(id: Long): Flow<Note?> {
+        return notes
+            .map { notes1 -> notes1.first { it.id == id } }
+            .flowOn(ioDispatcher)
+    }
+
+    override suspend fun delete(id: Long) {
+        withContext(ioDispatcher) {
+            notes.updateAndGet { notes1 ->
+
+                notes1.toMutableList().apply { removeAll { it.id == id } }
+            }
+        }
+    }
+}
