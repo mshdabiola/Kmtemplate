8976df4a21223c17ce5a9571482a47496372b5b1
mshdabiola <mshdabiola@gmail.com>
2024-12-16 16:59:44 +0100

    chore: Release 1.2.4This commit updates the version code and name for the application release to 1.2.4.Specifically, the following changes were made:- Updated `versionCode` and `versionName` in `fossReliantRelease-badging.txt`. - Updated the `version` variable in `MainApp.kt` to display the correct version in the window title. - Updated `versionCode` and `versionName` in `libs.versions.toml`.

--- app/src/jvmMain/kotlin/com/mshdabiola/skeletonapp/MainApp.kt
+++ app/src/jvmMain/kotlin/com/mshdabiola/skeletonapp/MainApp.kt
@@ -36,7 +36,7 @@ fun mainApp() {
             position = WindowPosition.Aligned(Alignment.Center),
         )
 
-        val version = "1.2.3"
+        val version = "1.2.4"
         Window(
             onCloseRequest = ::exitApplication,
             title = "$appName v$version",
