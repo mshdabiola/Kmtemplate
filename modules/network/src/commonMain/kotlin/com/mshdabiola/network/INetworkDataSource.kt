1498f0970136c8bcf882901a6dffb684e171107f
mshdabiola <mshdabiola@gmail.com>
2025-05-30 15:37:59 +0100

    Refactor: Clean up network layer code
    This commit refactors the network layer by:
    
    - Removing unused code and comments from `WikiAPI.kt` and `INetworkDataSource.kt`.
    - Fixing constructor calls in `Articles.kt` by removing unnecessary parentheses.
    - Improving parameter handling in `NetworkDataSource.kt` by using `parametersOf` and `plus` for building query parameters.
    - Enhancing error handling in `NetworkDataSource.kt` by specifically catching `IOException` and rethrowing it with more context.

--- modules/network/src/commonMain/kotlin/com/mshdabiola/network/INetworkDataSource.kt
+++ modules/network/src/commonMain/kotlin/com/mshdabiola/network/INetworkDataSource.kt
@@ -19,15 +19,6 @@ interface INetworkDataSource {
         offset: Int,
     ): Response
 
-    //
-//
-//    @GET("w/api.php?action=query&format=json&formatversion=2&generator=allcategories&prop=categoryinfo|description|pageimages&piprop=thumbnail&pithumbsize=70")
-//    fun searchCategoriesForPrefix(
-//        @Query("gacprefix") prefix: String?,
-//        @Query("gaclimit") itemLimit: Int,
-//        @Query("gacoffset") offset: Int
-//    ): Single<MwQueryResponse>
-//
     suspend fun getCategoriesByName(
         prefix: String,
         suffix: String,
@@ -35,35 +26,15 @@ interface INetworkDataSource {
         offset: Int,
     ): Response
 
-    //
-//    @GET("w/api.php?action=query&format=json&formatversion=2&generator=allcategories&prop=categoryinfo|description|pageimages&piprop=thumbnail&pithumbsize=70")
-//    fun getCategoriesByName(
-//        @Query("gacfrom") startingCategory: String?,
-//        @Query("gacto") endingCategory: String?,
-//        @Query("gaclimit") itemLimit: Int,
-//        @Query("gacoffset") offset: Int
-//    ): Single<MwQueryResponse>
-//
     suspend fun getSubCategoryList(
         categoryName: String,
         continuation: Map<String, String>,
     ): Response
 
-    //    @GET("w/api.php?action=query&format=json&formatversion=2&generator=categorymembers&gcmtype=subcat&prop=info&gcmlimit=50")
-//    fun getSubCategoryList(
-//        @Query("gcmtitle") categoryName: String,
-//        @QueryMap(encoded = true) continuation: Map<String, String>
-//    ): Single<MwQueryResponse>
-//
     suspend fun getParentCategoryList(
         categoryName: String,
         continuation: Map<String, String>,
     ): Response
-//    @GET("w/api.php?action=query&format=json&formatversion=2&generator=categories&prop=info&gcllimit=50")
-//    fun getParentCategoryList(
-//        @Query("titles") categoryName: String?,
-//        @QueryMap(encoded = true) continuation: Map<String, String>
-//    ): Single<MwQueryResponse>
 
     suspend fun getTimeline(
         limit: Int,
