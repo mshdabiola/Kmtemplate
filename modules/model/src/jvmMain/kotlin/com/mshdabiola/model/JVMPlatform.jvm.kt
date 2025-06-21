9bfbd0ba1464860b028f0bdeb8811b3de44c393d
mshdabiola <mshdabiola@gmail.com>
2025-06-21 17:25:54 +0000

    chore: Add license and copyright headers
    Add GNU Affero General Public License v3.0 headers to all source files.
    Update copyright years where necessary.

--- modules/model/src/jvmMain/kotlin/com/mshdabiola/model/JVMPlatform.jvm.kt
+++ modules/model/src/jvmMain/kotlin/com/mshdabiola/model/JVMPlatform.jvm.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025 MshdAbiola
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 package com.mshdabiola.model
 
 class JVMPlatform : Platform {
@@ -7,7 +24,8 @@ class JVMPlatform : Platform {
             val os =
                 if (operSys.contains("win")) {
                     "Windows"
-                } else if (operSys.contains("nix") || operSys.contains("nux") ||
+                } else if (operSys.contains("nix") ||
+                    operSys.contains("nux") ||
                     operSys.contains("aix")
                 ) {
                     "Linux"
--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) $YEAR AntsyLich and The Mihon Authors
+ * Copyright (C) $YEAR MshdAbiola
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
