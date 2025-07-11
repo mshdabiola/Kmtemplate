eb70651fbb9c1bbca50a148d112a7aabc30e8a73
mshdabiola <mshdabiola@gmail.com>
2025-07-11 20:07:57 +0100

    Refactor: Move color definitions and update theme
    - Move color definitions from `MyColors.kt` to `Color.kt`.
    - Delete `MyColors.kt`.
    - Update `Theme.kt` to use the new color definitions and remove `androidTheme` parameter.
    - Add contrast parameter to `KmtTheme`.
    - Update previews in `Background.kt` to remove `androidTheme` parameter.

--- modules/designsystem/src/commonMain/kotlin/com/mshdabiola/designsystem/component/Background.kt
+++ modules/designsystem/src/commonMain/kotlin/com/mshdabiola/designsystem/component/Background.kt
@@ -164,7 +164,7 @@ fun BackgroundDynamic() {
 @Preview
 @Composable
 fun BackgroundAndroid() {
-    KmtTheme(androidTheme = true) {
+    KmtTheme {
         KmtBackground(Modifier.size(100.dp), content = {})
     }
 }
@@ -188,7 +188,7 @@ fun GradientBackgroundDynamic() {
 @Preview
 @Composable
 fun GradientBackgroundAndroid() {
-    KmtTheme(androidTheme = true) {
+    KmtTheme {
         KmtGradientBackground(Modifier.size(100.dp), content = {})
     }
 }
