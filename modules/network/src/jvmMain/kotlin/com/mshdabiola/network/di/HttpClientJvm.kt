4bd80366c01fa69af050a4426efbc260d5100fd1
mshdabiola <mshdabiola@gmail.com>
2025-07-13 18:38:29 +0100

    refactor: simplify network module and remove unused models
    This commit refactors the network module by:
    - Removing unused data models: `Continue`, `Imageinfo`, `Model`, `Page`, `Query`, `Response`, `Thumbnail`.
    - Removing unused request classes: `Articles`, `Wiki`, `WikiAPI`.
    - Simplifying `NetworkDataSource` and `INetworkDataSource` to only include the `goToGoogle` function.
    - Renaming platform-specific HttpClient configuration files for consistency.
    - Consolidating JVM HttpClient configuration.

--- modules/network/src/jvmMain/kotlin/com/mshdabiola/network/di/HttpClientJvm.kt
+++ modules/network/src/jvmMain/kotlin/com/mshdabiola/network/di/HttpClientJvm.kt
@@ -15,23 +15,24 @@
  */
 package com.mshdabiola.network.di
 
+import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.HttpClientEngineConfig
-import io.ktor.client.plugins.cache.HttpCache
-import io.ktor.client.plugins.cache.storage.FileStorage
+import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
-import java.io.File
+
+actual val httpClient: HttpClient
+    get() =
+        HttpClient(CIO) {
+            init()
+        }
 
 actual fun <T : HttpClientEngineConfig> HttpClientConfig<T>.initPlatform() {
     install(Logging) {
         logger = Logger.SIMPLE
         level = LogLevel.ALL
-    }
-    install(HttpCache) {
-        val file = File.createTempFile("abiola", "tem")
-        publicStorage(FileStorage(file))
     }
 }
