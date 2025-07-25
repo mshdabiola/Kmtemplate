8482d04f77395e42c1f7969ecf5d0a0085d57568
mshdabiola <mshdabiola@gmail.com>
2025-07-25 20:25:34 +0100

    Refactor: Set DetailScreen background color
    This commit updates `DetailScreen.kt` to explicitly set the `containerColor` of the `Scaffold` to `MaterialTheme.colorScheme.surface`. This ensures the background color of the detail screen is consistent with the application's theme.

--- features/detail/src/commonMain/kotlin/com/mshdabiola/detail/DetailScreen.kt
+++ features/detail/src/commonMain/kotlin/com/mshdabiola/detail/DetailScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -86,6 +87,7 @@ internal fun DetailScreen(
                     },
                 )
             },
+            containerColor = MaterialTheme.colorScheme.surface,
         ) { paddingValues ->
             Column(
                 modifier = Modifier
