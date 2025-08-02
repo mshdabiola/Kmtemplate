168c500db55a689709b4fee9ba2ffadf50674d02
mshdabiola <mshdabiola@gmail.com>
2025-08-02 23:53:38 +0100

    Fix: Adjust FAB padding to avoid navigation bar overlap
    This commit modifies the `KmtScaffold.kt` file to prevent the Floating Action Button (FAB) from overlapping with the system navigation bar.
    
    - **`app/src/commonMain/kotlin/com/mshdabiola/kmtemplate/ui/KmtScaffold.kt`**:
        - Added `navigationBarsPadding()` modifier to the FAB to ensure it is positioned correctly above the navigation bar.

--- app/src/commonMain/kotlin/com/mshdabiola/kmtemplate/ui/KmtScaffold.kt
+++ app/src/commonMain/kotlin/com/mshdabiola/kmtemplate/ui/KmtScaffold.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -185,6 +186,7 @@ fun KmtScaffold(
                             Fab(
                                 appState = appState,
                                 modifier = Modifier
+                                    .navigationBarsPadding()
                                     .sharedBounds(
                                         sharedContentState = rememberSharedContentState("note_-1"),
                                         animatedVisibilityScope = this,
