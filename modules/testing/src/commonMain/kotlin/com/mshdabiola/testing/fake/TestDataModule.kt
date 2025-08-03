bf301d56dbf0cd01d29241ae99179a7edc9703d1
mshdabiola <mshdabiola@gmail.com>
2025-08-03 19:44:54 +0100

    Refactor: Update NetworkRepository binding in TestDataModule
    This commit updates the binding for `NetworkRepository` in the `TestDataModule`.
    It changes the bound type from `INetworkRepository` to `NetworkRepository`.
    
    - **`modules/testing/src/commonMain/kotlin/com/mshdabiola/testing/fake/TestDataModule.kt`**:
        - Changed `bind INetworkRepository::class` to `bind NetworkRepository::class` for `FakeNetworkRepository`.
        - Updated import from `com.mshdabiola.data.repository.INetworkRepository` to `com.mshdabiola.data.repository.NetworkRepository`.

--- modules/testing/src/commonMain/kotlin/com/mshdabiola/testing/fake/TestDataModule.kt
+++ modules/testing/src/commonMain/kotlin/com/mshdabiola/testing/fake/TestDataModule.kt
@@ -16,7 +16,7 @@
 package com.mshdabiola.testing.fake
 
 import com.mshdabiola.analytics.di.analyticsModule
-import com.mshdabiola.data.repository.INetworkRepository
+import com.mshdabiola.data.repository.NetworkRepository
 import com.mshdabiola.data.repository.NoteRepository
 import com.mshdabiola.data.repository.UserDataRepository
 import com.mshdabiola.testing.fake.repository.FakeNetworkRepository
@@ -30,7 +30,7 @@ import org.koin.dsl.module
 val testDataModule =
     module {
         includes(testDispatcherModule, analyticsModule)
-        singleOf(::FakeNetworkRepository) bind INetworkRepository::class
+        singleOf(::FakeNetworkRepository) bind NetworkRepository::class
         singleOf(::FakeNoteRepository) bind NoteRepository::class
         singleOf(::FakeUserDataRepository) bind UserDataRepository::class
     }
