c6c8e0e57c73a171fe24bcc9b0e0a1ca90c32998
mshdabiola <mshdabiola@gmail.com>
2025-08-18 13:20:06 +0100

    Feat: Enhance Notification model with type and duration
    This commit enhances the `Notification` sealed class to include `type` and `duration` properties.
    
    - **`core/model/src/commonMain/kotlin/com/mshdabiola/model/Notification.kt`**:
        - Added `type` (defaulting to `Type.Default`) and `duration` (defaulting to `SnackbarDuration.Short`) to the `Notification` sealed class and its subclasses (`Message`, `MessageWithAction`).
        - Introduced a new enum `Type` with values: `Default`, `Error`, `Success`, `Warning`.
        - Introduced a new enum `SnackbarDuration` with values: `Short`, `Long`, `Indefinite`.

--- core/model/src/commonMain/kotlin/com/mshdabiola/model/Notification.kt
+++ core/model/src/commonMain/kotlin/com/mshdabiola/model/Notification.kt
@@ -1,10 +1,29 @@
 package com.mshdabiola.model
 
-sealed class Notification {
-    data class Message(val message: String) : Notification()
+sealed class Notification(
+    open val type: Type= Type.Default,
+    open val duration: SnackbarDuration= SnackbarDuration.Short,
+
+    ) {
+    data class Message(
+        override val type: Type= Type.Default,
+        val message: String,
+        override val duration: SnackbarDuration= SnackbarDuration.Short,
+    ) : Notification(type,duration)
     data class MessageWithAction(
+        override val type: Type= Type.Default,
+        override val duration: SnackbarDuration= SnackbarDuration.Short,
         val message: String,
         val action: String,
         val actionCallback: () -> Unit,
-    ) : Notification()
+    ) : Notification(type,duration)
+}
+enum class Type{
+    Default, Error, Success, Warning
+}
+
+enum class SnackbarDuration {
+    Short,
+    Long,
+    Indefinite,
 }
