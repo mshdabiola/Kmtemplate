9b67160809056e0dfb58ee8de3576216a01d3183
mshdabiola <mshdabiola@gmail.com>
2025-08-22 20:41:53 +0100

    Add BuildConfig with version details
    This commit introduces a new `BuildConfig.kt` file.
    It defines `REVISION_CODE`, `VERSION_CODE_DESKTOP`, `VERSION_CODE`, and `VERSION_NAME` constants.

---
+++ core/model/src/commonMain/kotlin/com/mshdabiola/model/BuildConfig.kt
@@ -0,0 +1,8 @@
+package com.mshdabiola.model
+
+object BuildConfig {
+    const val REVISION_CODE = 1
+    const val VERSION_CODE_DESKTOP  = "1.2.16"
+    const val VERSION_CODE  = 17
+    const val VERSION_NAME = "1.2.16"
+}
