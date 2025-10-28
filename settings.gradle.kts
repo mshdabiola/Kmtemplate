7ec271252e10a4a0cff1d5a2c65f3e67b3f06ad5
mshdabiola <mshdabiola@gmail.com>
2025-10-28 15:57:03 +0100

    Add Foojay Toolchains Resolver Gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
